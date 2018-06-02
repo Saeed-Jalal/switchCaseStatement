f412b278ad6290f13600614ba196e5882e2b8c37
Saeed-Jalal <syedsaeedjalal@gmail.com>
2018-06-02 17:23:54 +0300

    How switch case work

---
+++ src/switchCaseStatement.java
@@ -0,0 +1,26 @@
+public class switchCaseStatement {
+    public static void main(String[]args){
+        int i=2;
+        switch(i)
+        {
+            case 1:
+                System.out.println("TalkMe1");
+                break;
+
+            case 2:
+                System.out.println("TalkMe2");
+                break;
+
+            case 3:
+                System.out.println("TalkMe3");
+                break;
+            case 4:
+                System.out.println("TalkMe4");
+                break;
+
+                default:
+                    System.out.println("Default");
+
+        }
+    }
+}
